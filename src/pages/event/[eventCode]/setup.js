b895c09e90d7956130c76773c29dc6d66c4504f6
Julian C <julian.curalli27@gmail.com>
2023-12-05 11:32:39 -0500

    added back to setup page button

--- src/pages/event/[eventCode]/setup.js
+++ src/pages/event/[eventCode]/setup.js
@@ -1,23 +1,39 @@
 import TeamList from "@/components/common/TeamList";
 import { getAuth, clerkClient } from "@clerk/nextjs/server";
 import { Tabs, Tab } from "@nextui-org/tabs";
+import { Button } from "@nextui-org/button";
 import prisma from "@prisma/index";
 import CategoryList from "@/components/common/CategoryList";
 import { categoryOptions } from "@/data/categories";
 import EventSetupForm from "@/components/common/forms/EventSetupForm";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { FaArrowLeftLong } from "react-icons/fa6";
 
 export default function EventSetupPage(props) {
   const { event } = props;
+  const router = useRouter();
   console.log(event.picks);
   return (
     <div className="z-10 pt-6 pb-12 flex flex-col max-w-5xl items-center justify-between gap-6">
       <Head>
         <title>Event Setup | Samosa Stats</title>
       </Head>
-      <p className="font-semibold text-2xl text-center">
-        {event.name} Setup Page
-      </p>
+      <div className="w-full flex flex-col px-12 gap-2">
+        <Button
+          variant="light"
+          startContent={<FaArrowLeftLong />}
+          onClick={() => {
+            router.push("/setup");
+          }}
+          className="justify-start max-w-fit"
+        >
+          Back to Setup
+        </Button>
+        <p className="font-semibold text-2xl text-center">
+          {event.name} Setup Page
+        </p>
+      </div>
       <Tabs aria-label="Setup Tabs" color="primary">
         <Tab key="teams" title="Teams">
           <TeamList eventCode={event.eventCode} />
